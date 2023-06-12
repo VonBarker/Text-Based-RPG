9e7e2bd5c0a413240586b3d4c02b57d3a2665c41
Barker_Von <barkv346@hsd.k12.or.us>
2023-06-12 12:13:02 -0700

    Added Looping when an Invalid Input is Submitted

--- src/main/java/com/rpg_example/App.java
+++ src/main/java/com/rpg_example/App.java
@@ -23,27 +23,35 @@ public final class App {
         String shipName = userInput.nextLine();
         playerShip.setName(shipName);
         
-        System.out.println("What ship type do you  want? The sloop is the smallest which makes it the fastest, but also the easiest to sink if hit. The brigantine is the medium size ship which makes it the middle ground in speed and how easly it can be sunk. The galleon is the largest ship which makes it the slowest, but it sinks the slowest. \n\nType 1 for Sloop, 2 for Brigantine, or 3 for Galleon");
-        String shipType = userInput.nextLine();
-        if(shipType.equals("1"))
+        boolean validInput = false;
+        while(!validInput)
         {
-            playerShip.setSize(1);
-            playerShip.setSpeed(3);
-            playerShip.setHealth(15);
+            System.out.println("What ship type do you  want? The sloop is the smallest which makes it the fastest, but also the easiest to sink if hit. The brigantine is the medium size ship which makes it the middle ground in speed and how easly it can be sunk. The galleon is the largest ship which makes it the slowest, but it sinks the slowest. \n\nType 1 for Sloop, 2 for Brigantine, or 3 for Galleon");
+            String shipType = userInput.nextLine();
+            if(shipType.equals("1"))
+            {
+                playerShip.setSize(1);
+                playerShip.setSpeed(3);
+                playerShip.setHealth(15);
+                validInput = true;
+            }
+            else if(shipType.equals("2"))
+            {
+                playerShip.setSize(1.5);
+                playerShip.setSpeed(2);
+                playerShip.setHealth(25);
+                validInput = true;
+            }
+            else if(shipType.equals("3"))
+            {
+                playerShip.setSize(2);
+                playerShip.setSpeed(1);
+                playerShip.setHealth(35);
+                validInput = true;
+            }
+            else System.out.println("Invalid Input");
         }
-        else if(shipType.equals("2"))
-        {
-            playerShip.setSize(1.5);
-            playerShip.setSpeed(2);
-            playerShip.setHealth(25);
-        }
-        else if(shipType.equals("3"))
-        {
-            playerShip.setSize(2);
-            playerShip.setSpeed(1);
-            playerShip.setHealth(35);
-        }
-        else System.out.println("Invalid Input");
+        validInput = false;
 
         playerShip.getType();
         System.out.println(playerShip);
